0776a0e18500c29d8626178f46e113f4bb23101b
HhhjtWSL <2821423807@qq.com>
2025-07-30 21:58:50 +0800

    fix

--- Code/Ch2/2_2.c
+++ Code/Ch2/2_2.c
@@ -11,13 +11,13 @@ void show_bytes(byte_pointer start, size_t len){
 }
 
 int main(){
-    short int v = -12345;
-    unsigned short uv = (unsigned short)v;   // 强制类型转换的结果保持位值不变，只是改变了解释这些位的方式 
-    printf("v = %d, uv = %u\n", v, uv);  
-    int x = -1;    // 0xFFFFFFFF 
-    printf("sizeof int = %ld\n",sizeof(int));
-    printf("sizeof short = %ld\n",sizeof(short));
-    printf("x = %u = %d\n",x,x);
+    // short int v = -12345;
+    // unsigned short uv = (unsigned short)v;   // 强制类型转换的结果保持位值不变，只是改变了解释这些位的方式 
+    // printf("v = %d, uv = %u\n", v, uv);  
+    // int x = -1;    // 0xFFFFFFFF 
+    // printf("sizeof int = %ld\n",sizeof(int));
+    // printf("sizeof short = %ld\n",sizeof(short));
+    // printf("x = %u = %d\n",x,x);
 
     short sx = -12345;
     unsigned short usx = sx;
